b23b7afa85434bccc8402bef4bd52edd89b57423
SvenVucetic <svucetic@tvz.hr>
2026-04-18 15:28:46 +0200

    Add files via upload

---
+++ lab1-1.php
@@ -0,0 +1,11 @@
+<?php 
+    $ime="Sven";
+    $prezime="Vučetić";
+    $rodenje="2005";
+
+    echo substr($ime,0,1);
+    echo ".";
+    echo $prezime;
+    echo "/";
+    echo substr($rodenje,-2);
+?>
